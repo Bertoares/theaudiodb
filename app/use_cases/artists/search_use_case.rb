0de6c652de17c3c4efd78525031da00232bde4da
Alberto Ares <alberto.ares@softwhisper.es>
2021-10-19 15:21:54 +0200

    se corrigen errores dela búsqueda en la api

--- app/use_cases/artists/search_use_case.rb
+++ app/use_cases/artists/search_use_case.rb
@@ -13,16 +13,19 @@ module Artists
 
             @searched_artist_bd = ::Artists::Search.new(name: @name).execute
 
-            unless @searched_artist_bd.any?               
+            unless @searched_artist_bd.any?   
+                #si no algo, si nada           
                 
                 results_api = ::Artists::SearchApi.new(name: @name).execute
 
-                if results_api
+                unless results_api['artists'] == nil 
+                    # si no es = nulo, si es != nulo
 
                     ::Artists::Create.new(results_api: results_api).execute
 
-        # realizo de nuevo la búsqueda para devolver, aunque sea 1, una LISTA y poder iterar el bucle del search
+    # realizo de nuevo la búsqueda para devolver, aunque sea 1, una LISTA y poder iterar el bucle del search
                     @searched_artist_bd = ::Artists::Search.new(name: @name).execute
+
                 end
                     
             end
